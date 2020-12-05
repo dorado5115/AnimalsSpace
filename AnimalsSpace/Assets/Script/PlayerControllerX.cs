094154dfd2ecb0aeea0c8f8e7a8932a18ffd8ca9
Sofia <doradodx@gmail.com>
2020-12-05 16:39:58 -0600

    BC mejorado

---
+++ AnimalsSpace/Assets/Script/PlayerControllerX.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver = false;
+    public Rigidbody2D rb;
+    private float moveSpeed = 3f;
+    new Vector2 movement;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver == false)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
