9886865287016a710b8e70c048e2f70487d40423
Sofia <doradodx@gmail.com>
2020-12-05 22:04:02 -0600

    mainmenu

---
+++ AnimalsSpace/Assets/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private int game;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        game = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    public void PlayGame()
+    {
+        StartCoroutine(Loadlevel(game));
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quitt");
+        Application.Quit();
+    }
+
+    IEnumerator Loadlevel(int level)
+    {
+        anim.SetTrigger("start");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(level);
+    }
+}
